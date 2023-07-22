cc7fd5533c9dd3476df57595835eb924f7d10372
Paolo Mosconi <paolo.mosconi@actvalue.com>
2023-07-22 16:05:51 +0200

    creazione api

---
+++ api/app.cjs
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+const express = require("express");
+const cors = require("cors");
+
+const PRODUCTS = [
+  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+];
+
+const port = 8000;
+
+const delay = sec => new Promise((resolve) => setTimeout(resolve, sec * 1000));
+
+const app = express();
+app.use(cors());
+
+app.get("/", async (req, res) => {
+  await delay(3);
+  res.send(PRODUCTS)
+})
+
+app.listen(port, () => console.log(`The server is listening on port ${port}`))
